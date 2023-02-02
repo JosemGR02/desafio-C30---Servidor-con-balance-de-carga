7020d7882757b86198e5001da520e0af1d8904f1
Jose gomez <100965428+JosemGR02@users.noreply.github.com>
2023-02-01 21:10:08 -0300

    actualiza3

--- src/Rutas/autenticacion/index.js
+++ src/Rutas/autenticacion/index.js
@@ -18,25 +18,35 @@ ruta.get("/login", estaAutenticado, (solicitud, respuesta) => {
 
 ruta.post("/login", passport.authenticate("login", { failureRedirect: "/api/autenticacion/error-login" }),
     (solicitud, respuesta) => {
-        respuesta.redirect("/");
+        respuesta.redirect("/api/autenticacion");
     }
 );
 
 // Registrarse
-ruta.get("/signup", (solicitud, respuesta) => {
-    respuesta.render("view/home", { email: solicitud.user.email });
+ruta.get("/signup", estaAutenticado, (solicitud, respuesta) => {
+    respuesta.render("view/signup");
 });
 
-ruta.post("/signup", passport.authenticate("signup", { failureRedirect: "/api/autenticacion/error-signup" }),
-    (solicitud, respuesta) => {
-        respuesta.redirect("/");
-    }
-);
+ruta.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/api/autenticacion', failureRedirect: '/api/autenticacion/error-signup'
+}));
+
 
 // Cerrar Sesion
 ruta.get("/logout", (solicitud, respuesta) => {
-    solicitud.logout();
-    respuesta.render("view/logout", { email: solicitud.respuestaUsuario });
+    try {
+        const { email } = solicitud.user;
+
+        solicitud.logout(error => {
+            if (error) {
+                respuesta.send(`${error}, Error al desloguearse`);
+            } else {
+                respuesta.render('view/logout', { email });
+            }
+        });
+    } catch (error) {
+        respuesta.send(`${error}, Error en el logout`);
+    }
 });
 
 
@@ -53,4 +63,3 @@ ruta.get("/error-signup", (solicitud, respuesta) => {
 
 
 export { ruta as RutAutenticacion };
-
